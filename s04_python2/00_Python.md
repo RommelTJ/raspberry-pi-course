60fda003057c2332b18a40da8ee028b56787b4e9
Rommel Rico <me@rommelrico.com>
2025-01-28 19:31:41 -0800

    :sparkles: s4: Conditions

---
+++ s04_python2/00_Python.md
@@ -0,0 +1,18 @@
+# Python Programming - Conditions, Loops, Lists
+
+## Conditions
+
+* See 01_conditions.py
+
+## Condition Operators
+
+## Validate User Input Exercise
+
+## Loops
+
+## Lists
+
+## Compute Max Value Inside a List Exercise
+
+## Python Modules
+
---
+++ s04_python2/01_conditions.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    temperature = 15
+
+    if temperature > 20:
+        print("It's hot outside!")
+    elif temperature > 10:
+        print("It's warm outside!")
+    elif temperature > 0:
+        print("It's cold outside!")
+    else:
+        print("It's freezing outside!")
