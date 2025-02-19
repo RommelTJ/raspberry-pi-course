666aee6a3351681eaf69edb8620ffa581bb22ba3
Rommel Rico <me@rommelrico.com>
2025-02-18 23:47:04 -0800

    :sparkles: s14: Detecting movement greater than 5s

--- s14_project/s14_project_sensor.py
+++ s14_project/s14_project_sensor.py
@@ -2,9 +2,8 @@ from os import path
 from gpiozero import LED, MotionSensor
 from signal import pause
 from picamzero import Camera
-from time import sleep
+from time import sleep, time
 
-# TODO: When the sensor detects motion greater than 5 seconds, it will turn on the LED
 # TODO: When the sensor detects motion greater than 5 seconds, take a photo and send an email
 # TODO: For each photo that we take, log the time and filename into a log file.
 
@@ -18,14 +17,29 @@ class MotionActivatedCamera:
         print("Initializing camera...")
         sleep(2) # Wait for the camera to initialize
         print("Camera initialized!")
-        self.pir_sensor.when_motion = self.turn_light_on
-        self.pir_sensor.when_no_motion = self.turn_light_off
+        self.pir_sensor.when_motion = self.on_motion_detected
+        self.pir_sensor.when_no_motion = self.on_motion_finished
+        self.time_of_first_motion = 0
+        self.last_photo_time = 0
+        self.MOVEMENT_THRESHOLD_SECONDS = 5
+        self.MIN_SECONDS_BETWEEN_PHOTOS = 30
 
-    def turn_light_on(self):
+    def on_motion_detected(self):
         self.light.on()
+        self.time_of_first_motion = time()
 
-    def turn_light_off(self):
+    def on_motion_finished(self):
         self.light.off()
+        motion_duration = time() - self.time_of_first_motion
+        print(f"Motion duration: {motion_duration} seconds")
+        if motion_duration > self.MOVEMENT_THRESHOLD_SECONDS:
+            can_take_photo = time() - self.last_photo_time > self.MIN_SECONDS_BETWEEN_PHOTOS
+            if can_take_photo:
+                self.take_photo()
+                self.last_photo_time = time()
+
+    def take_photo(self):
+        print("Taking photo...")
 
     def run(self):
         try:
